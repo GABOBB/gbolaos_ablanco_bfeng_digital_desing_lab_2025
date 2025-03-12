7d922305522e1553ebcc55a631a680fe2ed58c87
GABOBB <gabbol2002@gmail.com>
2025-03-11 23:18:36 -0600

    se agrega el testbench

--- problema_1/P1_tb.sv
+++ problema_1/P1_tb.sv
@@ -1,21 +1,41 @@
 module P1_tb();
-   logic a,b,z;
-	P1 modulo(a,b,z);
-	
+
+	logic [3:0] A;
+	logic [6:0] B;
+	logic [6:0] S;
+
+	// Instancia del módulo P1
+	P1 prueva1 (
+		.switches(A),
+		.seg_decenas(B),
+		.seg_unidades(S)
+	);
+
+	// Bloque de prueba
 	initial begin
+		A = 4'b1111;
+		#40;
+		
+		A = 4'b1110;  
+		#40; 
 	
-	a = 0;
-	b = 1;
-	
-	#40
-	
-	a = 0;
-	b = 0;
-	
-	#40 
-	a = 1;
-	b = 1;
-	
+		A = 4'b1011;  
+		#40;
+		
+		A = 4'b0011;  
+		#40;
+		
+		A = 4'b0001;
+		#40;
+		
+		A = 4'b1100;
+		#40;
+		
+		A = 4'b0101;
+		#40;
+		
+		A = 4'b0000;
+		$finish;
 	end
-	
+
 endmodule
